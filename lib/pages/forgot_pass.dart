9f0aece4dc460820d20fe2a8b43d1ebd08180e6c
Your Name <you@example.com>
2023-02-21 00:33:15 +0900

    13. Implementing Forgot Password Feature part 2

--- lib/pages/forgot_pass.dart
+++ lib/pages/forgot_pass.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_firebase_app/pages/login.dart';
+import 'package:flutter_firebase_app/pages/signup.dart';
 
 class ForgotPass extends StatefulWidget {
   const ForgotPass({super.key});
@@ -10,11 +13,160 @@ class ForgotPass extends StatefulWidget {
 }
 
 class _ForgotPassState extends State<ForgotPass> {
+  final _formKey = GlobalKey<FormState>();
+
+  String email = '';
+
+  TextEditingController emailController = TextEditingController();
+
+  Future<void> resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.blueGrey,
+          content: Text(
+            'send email please confirm',
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.amber,
+            ),
+          ),
+        ),
+      );
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'user-not-found') {
+        print('No user found for that email');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.blueGrey,
+            content: Text(
+              'No user found for that email',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.amber,
+              ),
+            ),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('forget_pass'),
+      backgroundColor: Colors.white,
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 20.0),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Image.asset('images/forget.jpg'),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20.0),
+                child: Text(
+                  'Reset link will be send to your email ID',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      fontSize: 20,
+                    ),
+                    border: OutlineInputBorder(),
+                    errorStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 15,
+                    ),
+                  ),
+                  controller: emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'please Enter email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter valid email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            email = emailController.text;
+                          });
+                          resetPassword();
+                        }
+                      },
+                      child: Text(
+                        'send Email',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Do not have an account?'),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Signup(),
+                          ),
+                        );
+                      },
+                      child: Text('Signup'),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
