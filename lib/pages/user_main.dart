5e95ea645759cda365395a136a82e9293042094f
Your Name <you@example.com>
2023-02-21 12:49:36 +0900

    14. User Main Page

--- lib/pages/user_main.dart
+++ lib/pages/user_main.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_firebase_app/user/change_pass.dart';
+import 'package:flutter_firebase_app/user/dashboard.dart';
+import 'package:flutter_firebase_app/user/profile.dart';
 
 class UserMain extends StatefulWidget {
   const UserMain({super.key});
@@ -9,8 +13,34 @@ class UserMain extends StatefulWidget {
 }
 
 class _UserMainState extends State<UserMain> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Dashboard(),
+    Profile(),
+    ChangePass(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Dashboard'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Change Password'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
---
+++ lib/user/change_pass.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ChangePass extends StatefulWidget {
+  const ChangePass({super.key});
+
+  @override
+  State<ChangePass> createState() => _ChangePassState();
+}
+
+class _ChangePassState extends State<ChangePass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
